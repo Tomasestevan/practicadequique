44f144f297a9bb21e6658ca0dfaec8cd99c8dfce
Tomasestevan <tomasestevan1997@gmail.com>
2021-01-22 18:14:28 -0300

    clase humano agregada

---
+++ humano.java
@@ -0,0 +1,14 @@
+package com.company;
+ import java.util.List;
+public class humano {
+    public String Nombre;
+    public String Apellido;
+    public List<Mascota> mascotas;
+
+    public humano (String nombre, String apellido){
+        Nombre = nombre;
+        Apellido = apellido;
+    }
+
+
+}
